90334e2d9db8a005255af8feeb5c1b1b03efcc50
h.rass1991 <h.rass199@gmail.com>
2025-02-13 01:06:57 +0330

    some boilerplate code

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func ReadCSV(filename string, ch chan datasource.User, wg *sync.WaitGroup) {
 	reader := csv.NewReader(file)
 	data, err := reader.ReadAll()
 	if err != nil {
+		fmt.Println("Error reading data from csv:", err)
 		panic(err)
 	}
 
@@ -35,28 +36,30 @@ func ReadCSV(filename string, ch chan datasource.User, wg *sync.WaitGroup) {
 		date, err := time.Parse("2006/01/02", dateString)
 		if err != nil {
 			fmt.Println("Error parsing date:", err)
-			continue
+			return
 		}
 		u.Birthdate = date
-		ch <- u
 
+		ch <- u
 	}
+
 	close(ch)
 }
 
-func CountUsersAbove30(ch chan datasource.User, wg *sync.WaitGroup) int {
+func CountUsers(ch chan datasource.User, wg *sync.WaitGroup) {
 	defer wg.Done()
-	usersAbove30 := 0
+	users := 0
 	for user := range ch {
 		age := time.Now().Year() - user.Birthdate.Year()
 		if time.Now().YearDay() < user.Birthdate.YearDay() {
 			age--
 		}
 		if age > 30 {
-			usersAbove30++
+			users++
+			fmt.Println("User above 30:", user.FirstName, user.LastName)
 		}
 	}
-	return usersAbove30
+	fmt.Println("Total users above 30:", users)
 }
 
 func main() {
@@ -65,20 +68,7 @@ func main() {
 	wg.Add(1)
 	go ReadCSV("storage/user.csv", userChan, &wg)
 
-	go func() {
-		wg.Wait()
-		close(userChan)
-	}()
-
-	for user := range userChan {
-		if time.Now().Year()-user.Birthdate.Year() > 30 {
-			fmt.Println("User above 30:", user.FirstName, user.LastName)
-		}
-	}
-
-	go func() {
-		usersAbove30 := CountUsersAbove30(userChan, &wg)
-		fmt.Println("users above 30: ", usersAbove30)
-	}()
-
+	wg.Add(1)
+	go CountUsers(userChan, &wg)
+	wg.Wait()
 }
